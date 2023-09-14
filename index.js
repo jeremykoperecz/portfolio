9ebed55fbc32a0f54df3b9757a578c838486946e
jeremykoperecz <kopereczjeremy@gmail.com>
2023-09-14 15:58:10 +0200

    modif js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const links = document.querySelectorAll("nav li");
 
+
+
+
 icons.addEventListener("click", () => {
     nav.classList.toggle("active");
-})
+});
 
 links.forEach(link => {
     link.addEventListener("click", () => {
         nav.classList.remove("active");
     });
 });
+
+
+
+const tests = document.querySelectorAll(".portfolio_exemple img")
+
+tests.forEach(test => {
+    test.addEventListener("mouseover", () => {
+        test.classList.add("test2");
+        test.addEventListener("mouseout", () => {
+            test.classList.remove("test2");
+        });
+    });
+
+})
